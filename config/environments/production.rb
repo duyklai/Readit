6afbde6b5956839640f8bd231e0fc03c0f89a4c4
Duy Lai <duyklai@gmail.com>
2019-11-16 04:40:45 -0600

    Preparing for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # To serve static assets
+  config.serve_static_assets = true
+  
 end
