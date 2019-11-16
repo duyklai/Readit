c004c44db7f2a57f2627fea8a8e3d147604f58b8
Duy Lai <duyklai@gmail.com>
2019-11-16 04:54:41 -0600

    Remove test

--- spec/controllers/home_page_controller_spec.rb
+++ spec/controllers/home_page_controller_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe HomePageController, type: :controller do
-  describe "home page" do
-    it 'gets home url' do
-        get home_page_home_url
-        assert_response :success
-    end
-  end
+    
 end
