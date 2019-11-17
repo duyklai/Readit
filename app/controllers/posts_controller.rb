3291e0e0f32ed168ed5f0c5240e43737542860c5
Duy Lai <duyklai@gmail.com>
2019-11-17 15:06:31 -0600

    Display posts on home page; Add title and body to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:user_id, :points, :tag)
+      params.require(:post).permit(:user_id, :points, :tag, :title, :body)
     end
 end
