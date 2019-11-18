2e90b473d4f0428034dee41e6d72af676a80c01a
Duy Lai <duyklai@gmail.com>
2019-11-18 16:56:24 -0600

    Add basic signup/signin/signout system

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
   has_many :comments
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
   root 'home_page#home'
   get 'home_page/home'
