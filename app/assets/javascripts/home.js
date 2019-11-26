b8c18f826595b99304f9f653922290c6e0a89909
Duy Lai <duyklai@gmail.com>
2019-11-26 16:26:00 -0600

    Add condition to when to addeventlistener to avoid console error

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,5 @@
 document.addEventListener("turbolinks:load", function() {
+  if (!document.querySelector(".create-post-form")) return;
   create_post_form = document.querySelector(".create-post-form");
   create_post_form.addEventListener("click", function(e) {
     location.href = "posts/new";
