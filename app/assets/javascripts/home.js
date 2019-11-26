47db7f58fa801ceed0b0dc35703e444a4fad6e01
Duy Lai <duyklai@gmail.com>
2019-11-26 16:23:24 -0600

    Change eventlistener initial condition to work with turbolink

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-  create_post_form = document.querySelector('.create-post-form');
-  create_post_form.addEventListener('click', function(e) {
-      location.href = "posts/new"
-  })
+document.addEventListener("turbolinks:load", function() {
+  create_post_form = document.querySelector(".create-post-form");
+  create_post_form.addEventListener("click", function(e) {
+    location.href = "posts/new";
+  });
 });
-  
