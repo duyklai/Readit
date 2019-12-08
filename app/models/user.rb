17aff9ff2c817c9d055ec7c3828ec2b6828dbbcb
Duy Lai <duyklai@gmail.com>
2019-12-07 20:39:55 -0600

    Add two function to total points of posts and comments of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
+
+  def post_total
+    total_points = 0
+    posts.each do |post|
+      total_points = total_points + post.points
+    end
+    return total_points
+  end
+
+  def comment_total
+    total_points = 0
+    comments.each do |comment|
+      total_points = total_points + comment.points
+    end
+    return total_points
+  end
+
 end
