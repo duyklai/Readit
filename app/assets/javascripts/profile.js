5ec33270dd144d85844d228ab705f69e3ecd9807
Duy Lai <duyklai@gmail.com>
2019-12-10 22:14:10 -0600

    Add styling for profile page for users; Add js for tab changes

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,22 @@
+document.addEventListener("turbolinks:load", function() {
+  if (document.querySelector(".column")) {
+    document.getElementById("default").click();
+  }
+});
+
+function profileSelectView(e, viewName) {
+  var i, viewcontent, tabs;
+  // Hiding all of the content
+  viewcontent = document.getElementsByClassName("view");
+  for (i = 0; i < viewcontent.length; i++) {
+    viewcontent[i].style.display = "none";
+  }
+  // Removing all of active
+  tabs = document.getElementsByClassName("tabs");
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].className = tabs[i].className.replace(" active", "");
+  }
+  // Showing the clicked element; Add active to clicked tab
+  document.getElementById(viewName).style.display = "block";
+  e.currentTarget.className += " active";
+}
