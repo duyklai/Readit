30ffb496c98823f132990413442c1c564c24885d
Duy Lai <duyklai@gmail.com>
2019-12-14 17:04:35 -0600

    Move ajax function to appropriate JS

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,26 +5,3 @@ document.addEventListener("turbolinks:load", function() {
     location.href = "posts/new";
   });
 });
-
-function submitCommentForm(post_id, user_id) {
-  // Get input form from comments/new.html.erb form
-  Rails.ajax({
-    url: `/comments/new?post_id=${post_id}`,
-    type: "GET",
-    success: function(response) {
-      document.body.append(response.documentElement);
-      // Insert values into the fields
-      let comment_form = document.querySelector(".comment_form");
-      comment_form.childNodes.forEach(input => {
-        if (input.id == "comment_user_id") {
-          input.value = user_id;
-        } else if (input.id == "comment_post_id") {
-          input.value = post_id;
-        }
-      });
-    },
-    error: function() {
-      alert("Error. Try again");
-    }
-  });
-}
