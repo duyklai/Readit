8d22ec9858d67927b0755f2cf8faeae4627816a3
Duy Lai <duyklai@gmail.com>
2020-01-18 00:06:28 -0600

    Able to create new comment with blank ancestry to be replace with nil

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
+  # Filling the blank ancestry attribute with nil
+  before_validation do
+    self.ancestry = nil if self.ancestry.blank?
+  end
   validates :body, presence: true
   belongs_to :post, optional: true
   belongs_to :user, optional: true
