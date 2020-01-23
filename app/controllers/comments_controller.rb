423043bc04d207092ef3592fa04d9b5de91cc580
Duy Lai <duyklai@gmail.com>
2020-01-23 17:04:03 -0600

    Change redirect back to post page after actions concerning comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,6 +30,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
+        redirect_to @comment.post and return
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -44,6 +45,7 @@ class CommentsController < ApplicationController
       if @comment.update(comment_params)
         format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
+        redirect_to @comment.post and return
       else
         format.html { render :edit }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -58,6 +60,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
+      redirect_to @comment.post and return
     end
   end
 
