5d2b5d794c1520fc516b402a041c344dfe5df3f9
Duy Lai <duyklai@gmail.com>
2020-02-03 18:35:33 -0600

    Add relationship to vote and post; Vote dependent on post on destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
   has_many :comments
-  belongs_to :user, optional: true
+  has_many :votes, dependent: :destroy
+  belongs_to :user
 end
