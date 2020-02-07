5903c1093cae1f8abd7b8747d233f8e6e00efbc6
Duy Lai <duyklai@gmail.com>
2020-02-06 19:05:06 -0600

    Add custom function for creating and attaching vote model when voting on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:show, :index, :points_up, :points_down]
+  before_action :authenticate_user!, except: [:show, :index]
 
   # GET /posts
   # GET /posts.json
@@ -64,30 +64,60 @@ class PostsController < ApplicationController
 
   def points_up
     @post = Post.find(params[:id])
-    @post.update_attributes(points: (@post.points + 1))
-    if @post.save
-      respond_to do |format|
-        format.html { redirect_to root_path}
-        format.js
+    if vote("up", current_user.id, @post.id)
+      @post.update_attributes(points: (@post.points + 1))
+      if @post.save
+        respond_to do |format|
+          format.html { redirect_to request.referrer}
+          format.js
+        end
+      else
+        flash[:notice] = "Error Voting Please Try Again"
+        redirect_to request.referrer
       end
     else
-      flash[:notice] = "Error Voting Please Try Again"
-      redirect_to root_path
+      flash[:notice] = ""
+      redirect_to request.referrer
     end
   end
 
   def points_down
     @post = Post.find(params[:id])
-    @post.update_attributes(points: (@post.points - 1))
-    if @post.save
-      respond_to do |format|
-        format.html { redirect_to root_path}
-        format.js
+    if vote("down", current_user.id , @post.id)
+      @post.update_attributes(points: (@post.points - 1))
+      if @post.save
+        respond_to do |format|
+          format.html { redirect_to request.referrer}
+          format.js
+        end
+      else
+        flash[:notice] = "Error Voting Please Try Again"
+        redirect_to request.referrer
       end
     else
-      flash[:notice] = "Error Voting Please Try Again"
-      redirect_to root_path
+      flash[:notice] = ""
+      redirect_to request.referrer
     end
+  end
+
+  # Custom function to find/create new vote record of the post/comment being voted on
+  # Returns true if vote has been change/new Vote record; Returns false if vote has not been changed
+  def vote(value, user, post = nil, comment = nil)
+    if Vote.where(:user_id => user, :post_id => post, :comment_id => comment).first
+      vote = Vote.where(:user_id => user, :post_id => post, :comment_id => comment).first
+    else
+      vote = Vote.new(value: value, user_id: user, post_id: post, comment_id: comment)
+      vote.save
+      return true
+    end
+
+    if vote.value != value
+      vote.value = value
+      vote.save
+      return true
+    end
+
+    return false
   end
 
   private
