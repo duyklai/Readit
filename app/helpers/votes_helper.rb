c54568ebee603e962cb2c43871c44b29eccfd670
Duy Lai <duyklai@gmail.com>
2020-02-11 17:14:42 -0600

    Add comment to explain what function will return

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,4 +1,5 @@
 module VotesHelper
+  # Returns true if user has voted on this post; false otherwise
   def checkVoted(user = nil, post = nil, comment = nil)
     if Vote.where(:user_id => user, :post_id => post, :comment_id => comment).first
       return true
