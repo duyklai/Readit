f17e7e9e6b98d3bf469b15f6ac2990940127cae3
Duy Lai <duyklai@gmail.com>
2020-02-11 17:15:50 -0600

    Move custom function to check if user have voted to model to avoid DRY

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,24 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :post, optional: true
   belongs_to :comment, optional: true
+
+  # Custom function to find/create new vote record of the post/comment being voted on
+  # Returns true if vote has been change/new Vote record; Returns false if vote has not been changed
+  def self.vote(value, user, post = nil, comment = nil)
+    if Vote.where(:user_id => user, :post_id => post, :comment_id => comment).first
+      vote = Vote.where(:user_id => user, :post_id => post, :comment_id => comment).first
+    else
+      vote = Vote.new(value: value, user_id: user, post_id: post, comment_id: comment)
+      vote.save
+      return "new"
+    end
+
+    if vote.value != value
+      vote.value = value
+      vote.save
+      return true
+    end
+
+    return false
+  end
 end
