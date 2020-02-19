81e7af310289ea9bc080e515028851d83d17031f
Duy Lai <duyklai@gmail.com>
2020-02-19 17:07:33 -0600

    Remove requirement of having a body in creation of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, :body, presence: true
+  validates :title, presence: true
   has_many :comments
   has_many :votes, dependent: :destroy
   belongs_to :user
