73284ef9c3fc40a9f5d076d7eca827dfe93fb6a7
Duy Lai <duyklai@gmail.com>
2020-02-27 21:19:49 -0600

    Client side javsascript restriction on tag name input from user

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -2,4 +2,14 @@ document.addEventListener("turbolinks:load", function() {
   if (document.querySelector(".tag-page")) {
     document.getElementById("default").click();
   }
+
+  // Removing spaces and special characters from tag creation
+  if (document.querySelector("#post_tag")) {
+    document.querySelector("#post_tag").onkeypress = function(e) {
+      if (
+        !((e.which >= 65 && e.which <= 90) || (e.which >= 97 && e.which <= 122))
+      )
+        return false;
+    };
+  }
 });
