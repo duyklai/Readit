2e4cba9a095403a9ba03d1e2f30f79eb292c5c03
Duy Lai <duyklai@gmail.com>
2020-02-28 18:43:02 -0600

    Add an extra column/attribute to user model for admin privilege

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_234557) do
+ActiveRecord::Schema.define(version: 2020_02_28_223830) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_02_24_234557) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
