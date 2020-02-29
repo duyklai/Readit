ec808994c43ac189f573ef540f5276e1c219976a
Duy Lai <duyklai@gmail.com>
2020-02-28 18:37:55 -0600

    Add necessary methods to user controller.rb for edit and deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,40 @@
 class UsersController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+        redirect_to @user and return
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+      redirect_to root_path and return
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:username, :email, :admin)
   end
 end
