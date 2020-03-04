06afe9174f5e91b5743f37a1c9178b54e5aa3de2
Duy Lai <duyklai@gmail.com>
2020-03-04 16:41:43 -0600

    Add javascripts to hide and remove notice after fade out

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,14 @@
 document.addEventListener("turbolinks:load", function() {
+  if (document.querySelector(".notice_div")) {
+    setTimeout(function() {
+      document.querySelector(".notice_div").classList.add("hide");
+    }, 1500);
+
+    setTimeout(function() {
+      document.querySelector(".notice_div").classList.add("remove");
+    }, 2000);
+  }
+
   if (!document.querySelector(".create-post-form")) return;
   create_post_form = document.querySelector(".create-post-form");
   create_post_form.addEventListener("click", function(e) {
