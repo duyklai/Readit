41215252feac1781df8eaa3998a080ff90866f93
Duy Lai <duyklai@gmail.com>
2020-03-04 16:42:26 -0600

    Adjust querySelector id name to fit database column name

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -4,8 +4,8 @@ document.addEventListener("turbolinks:load", function() {
   }
 
   // Removing spaces and special characters from tag creation
-  if (document.querySelector("#post_tag")) {
-    document.querySelector("#post_tag").onkeypress = function(e) {
+  if (document.querySelector("#post_tag_id")) {
+    document.querySelector("#post_tag_id").onkeypress = function(e) {
       if (
         !((e.which >= 65 && e.which <= 90) || (e.which >= 97 && e.which <= 122))
       )
