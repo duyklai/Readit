c3bbf55135a81a789114f366694332164620c71a
Duy Lai <duyklai@gmail.com>
2020-03-23 18:45:20 -0500

    Add uniqueness to username of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true
+  validates_uniqueness_of :username
   has_many :posts
   has_many :comments
   has_many :votes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_223830) do
+ActiveRecord::Schema.define(version: 2020_03_23_225630) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
