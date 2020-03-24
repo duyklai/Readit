f7547735123dae5b734bb303957c4abc3df794b2
Duy Lai <duyklai@gmail.com>
2020-03-24 17:47:05 -0500

    Add basic GET test for homepage

--- spec/controllers/home_page_controller_spec.rb
+++ spec/controllers/home_page_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HomePageController, type: :controller do
-    
+  describe "GET #home" do
+    it "returns a success response" do
+      get :home, params: {}
+      expect(response).to be_successful
+    end
+  end
 end
