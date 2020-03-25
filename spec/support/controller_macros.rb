79ca4f461ca8c7d9d7cc6baaea5acdf6c7daea9d
Duy Lai <duyklai@gmail.com>
2020-03-24 19:04:27 -0500

    Add username attribute to user factory

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -9,6 +9,7 @@ module ControllerMacros
 
   FactoryBot.define do
     factory :user do
+      username { "username" }
       email { Faker::Internet.email }
       password { "password"} 
       password_confirmation { "password" }
