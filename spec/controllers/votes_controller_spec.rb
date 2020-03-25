af665cb9077e89c321585ad7a1504a61e340bed8
Duy Lai <duyklai@gmail.com>
2020-03-25 17:16:44 -0500

    Remove unnecessary testing (invalid and new/edit/show); Add new testing for comment and post votes

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -28,12 +28,15 @@ RSpec.describe VotesController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Vote. As you add validations to Vote, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+  let(:valid_attributes_post) {
+    { value: 'up', user_id: 1, post_id: 1, comment_id: nil }
+  }
+  let(:valid_attributes_comment) {
+    { value: 'up', user_id: 1, post_id: nil, comment_id: 1 }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { value: nil, user_id: nil, post_id: nil, comment_id: nil }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -41,100 +44,116 @@ RSpec.describe VotesController, type: :controller do
   # VotesController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "returns a success response" do
-      Vote.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET #show" do
-    it "returns a success response" do
-      vote = Vote.create! valid_attributes
-      get :show, params: {id: vote.to_param}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns a success response" do
-      vote = Vote.create! valid_attributes
-      get :edit, params: {id: vote.to_param}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
+  login_user
 
   describe "POST #create" do
-    context "with valid params" do
+    context "with valid params for post" do
       it "creates a new Vote" do
         expect {
-          post :create, params: {vote: valid_attributes}, session: valid_session
+          post :create, params: {vote: valid_attributes_post}, session: valid_session
         }.to change(Vote, :count).by(1)
       end
 
-      it "redirects to the created vote" do
-        post :create, params: {vote: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(Vote.last)
+      it "redirects to the home page" do
+        post :create, params: {vote: valid_attributes_post}, session: valid_session
+        expect(response).to redirect_to(root_path)
       end
     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {vote: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
+    context "with valid params for comment" do
+      let(:post_original) { Post.create(user_id: 1, points: 1, tag_id: 1, title: "newTitle", body: "newBody") }
+      let(:comment) { Comment.create(user_id: 1, post_id: post_original.id, points: 1, body: "newBody", ancestry: nil) }
+      let(:valid_attributes_comment) {
+        { value: 'up', user_id: 1, post_id: nil, comment_id: comment.id }
+      }
+
+      it "creates a new Vote" do
+        expect {
+          post :create, params: {vote: valid_attributes_comment}, session: valid_session
+        }.to change(Vote, :count).by(1)
+      end
+
+      it "redirects to the home page due to creating vote from home page" do
+        post :create, params: {vote: valid_attributes_comment}, session: valid_session
+        expect(response).to redirect_to(root_path)
       end
     end
   end
 
   describe "PUT #update" do
+    let(:post_original) { Post.create(user_id: 1, points: 1, tag_id: 1, title: "newTitle", body: "newBody") }
+    let(:comment) { Comment.create(user_id: 1, post_id: post_original.id, points: 1, body: "newBody", ancestry: nil) }
+
     context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+      let(:new_attributes_post) {
+        { value: 'down', user_id: 1, post_id: post_original.id, comment_id: nil }
       }
 
-      it "updates the requested vote" do
-        vote = Vote.create! valid_attributes
-        put :update, params: {id: vote.to_param, vote: new_attributes}, session: valid_session
+      it "updates the requested post vote " do
+        vote = Vote.create! valid_attributes_post
+        put :update, params: {id: vote.to_param, vote: new_attributes_post}, session: valid_session
         vote.reload
-        skip("Add assertions for updated state")
+        expect(vote.value).to eq('down')
+        expect(vote.user_id).to eq(1)
+        expect(vote.post_id).to eq(1)
+        expect(vote.comment_id).to eq(nil)
       end
 
-      it "redirects to the vote" do
-        vote = Vote.create! valid_attributes
-        put :update, params: {id: vote.to_param, vote: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(vote)
+      it "redirects to the home page" do
+        vote = Vote.create! valid_attributes_post
+        put :update, params: {id: vote.to_param, vote: valid_attributes_post}, session: valid_session
+        expect(response).to redirect_to(root_path)
       end
     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
-        vote = Vote.create! valid_attributes
-        put :update, params: {id: vote.to_param, vote: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
+    context "with valid params for comment votes" do
+      let(:new_attributes_comment) {
+        { value: 'down', user_id: 1, post_id: nil, comment_id: comment.id }
+      }
+
+      it "updates the requested comment vote " do
+        vote = Vote.create! valid_attributes_comment
+        put :update, params: {id: vote.to_param, vote: new_attributes_comment}, session: valid_session
+        vote.reload
+        expect(vote.value).to eq('down')
+        expect(vote.user_id).to eq(1)
+        expect(vote.post_id).to eq(nil)
+        expect(vote.comment_id).to eq(1)
+      end
+
+      it "redirects to the home page due to creating vote from home page" do
+        vote = Vote.create! valid_attributes_comment
+        put :update, params: {id: vote.to_param, vote: valid_attributes_comment}, session: valid_session
+        expect(response).to redirect_to(root_path)
       end
     end
   end
 
   describe "DELETE #destroy" do
-    it "destroys the requested vote" do
-      vote = Vote.create! valid_attributes
+    it "destroys the requested post vote" do
+      vote = Vote.create! valid_attributes_post
       expect {
         delete :destroy, params: {id: vote.to_param}, session: valid_session
       }.to change(Vote, :count).by(-1)
     end
 
-    it "redirects to the votes list" do
-      vote = Vote.create! valid_attributes
+    it "redirects to the home page" do
+      vote = Vote.create! valid_attributes_post
       delete :destroy, params: {id: vote.to_param}, session: valid_session
-      expect(response).to redirect_to(votes_url)
+      expect(response).to redirect_to(root_path)
+    end
+
+    it "destroys the requested comment vote" do
+      vote = Vote.create! valid_attributes_comment
+      expect {
+        delete :destroy, params: {id: vote.to_param}, session: valid_session
+      }.to change(Vote, :count).by(-1)
+    end
+
+    it "redirects to the home page" do
+      vote = Vote.create! valid_attributes_comment
+      delete :destroy, params: {id: vote.to_param}, session: valid_session
+      expect(response).to redirect_to(root_path)
     end
   end
 
