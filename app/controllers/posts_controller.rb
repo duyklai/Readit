9387d1118962da0a71d667e0fdbe4851bf36e39b
Duy Lai <duyklai@gmail.com>
2020-03-24 19:03:11 -0500

    Remove index method; Adjust redirects

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:show, :index]
-
-  # GET /posts
-  # GET /posts.json
-  def index
-    @posts = Post.all
-  end
+  before_action :authenticate_user!, except: [:show]
 
   # GET /posts/1
   # GET /posts/1.json
@@ -34,7 +28,6 @@ class PostsController < ApplicationController
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
-        redirect_to root_path and return
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -64,9 +57,8 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
-      redirect_to root_path
     end
   end
 
