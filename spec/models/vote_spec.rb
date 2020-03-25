5f5cdcad287ecd7a1ad66884344e11a89cd7f6ca
Duy Lai <duyklai@gmail.com>
2020-03-25 17:17:46 -0500

    Change vote 'value' to appropriate type

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  let(:vote_post) { Vote.new(value: 1, user_id: 1, post_id: 1, comment_id: nil) }
-  let(:vote_comment) { Vote.new(value: 1, user_id: 1, post_id: nil, comment_id: 1) }
+  let(:vote_post) { Vote.new(value: 'up', user_id: 1, post_id: 1, comment_id: nil) }
+  let(:vote_comment) { Vote.new(value: 'up', user_id: 1, post_id: nil, comment_id: 1) }
 
   it 'post is valid' do
     expect(vote_post).to be_valid
