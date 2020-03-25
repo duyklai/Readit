41f78f3404a52dd25c76804d4a7a7c3365c5a452
Duy Lai <duyklai@gmail.com>
2020-03-25 17:13:26 -0500

    Add feature to authenticate user before interacting with comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /comments/new
   def new
