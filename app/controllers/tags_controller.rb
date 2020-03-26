79ef0bf5caf568d3386a1b5ba7f55b2a76371b96
Duy Lai <duyklai@gmail.com>
2020-03-25 19:34:24 -0500

    Remove unused method

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
-  before_action :set_tag, only: [:show, :edit, :update, :destroy]
+  before_action :set_tag, only: [:show, :update, :destroy]
 
   # GET /tags/1
   # GET /tags/1.json
