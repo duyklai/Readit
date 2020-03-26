98cccaa04dbe33f0d8669c845cb36cab12353f4f
Duy Lai <duyklai@gmail.com>
2020-03-25 19:38:04 -0500

    Update test conditions; very basic request testing

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
   describe "GET /comments" do
+    let(:post) { Post.create(user_id: 1, points: 1, tag_id: 1, title: "newTitle", body: "newBody") }
     it "works! (now write some real specs)" do
-      get comments_path
+      get post_path(post)
       expect(response).to have_http_status(200)
     end
   end
--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Tags", type: :request do
   describe "GET /tags" do
+    let(:tag) { Tag.create(:name => "newtag") }
     it "works! (now write some real specs)" do
-      get tags_path
+      get tag_path(tag)
       expect(response).to have_http_status(200)
     end
   end
