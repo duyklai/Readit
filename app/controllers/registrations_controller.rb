1968d7d8a9c3d0191158dae7c3328670d430b438
Duy Lai <duyklai@gmail.com>
2020-04-01 18:25:27 -0500

    Override the delete function to check for other admin before deleting admin account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
 
+  def destroy
+    if current_user.admin? # If current user is admin, check
+      if User.where(admin: true).count > 1 # If there is more than 1 admin, allow delete account
+        resource.destroy
+        respond_to do |format|
+          format.html { redirect_to root_path, notice: 'User was successfully deleted.' }
+          format.json { head :no_content }
+        end
+      else # There is 1 admin
+        flash[:alert] = "You are the only admin. Please make someone else admin before deleting your account."
+        redirect_to request.referrer
+      end
+    else # If current user not admin
+      resource.destroy
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: 'User was successfully deleted.' }
+        format.json { head :no_content }
+      end
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
@@ -19,4 +40,5 @@ class RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     user_path(resource)
   end
+
 end
