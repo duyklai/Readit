2c27e5939ca50b898300d4d433a845c2edcd4cf6
Duy Lai <duyklai@gmail.com>
2020-04-01 18:27:47 -0500

    Comments are now dependent on the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :title, :user_id, presence: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   belongs_to :user, optional: true
   belongs_to :tag, optional: true
