240c4039158f63303b6ce76fc3fa3e7bad2a5a72
Duy Lai <duyklai@gmail.com>
2020-04-02 17:54:24 -0500

    Post and comments are now dependent on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates_uniqueness_of :username
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :votes
 
   def post_total
