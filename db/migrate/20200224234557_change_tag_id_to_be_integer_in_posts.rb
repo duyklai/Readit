4117085c803c125ebdb70213aa3cfa290a6b0da1
Duy Lai <duyklai@gmail.com>
2020-04-07 15:27:22 -0500

    Update migration syntax

--- db/migrate/20200224234557_change_tag_id_to_be_integer_in_posts.rb
+++ db/migrate/20200224234557_change_tag_id_to_be_integer_in_posts.rb
@@ -1,9 +1,5 @@
 class ChangeTagIdToBeIntegerInPosts < ActiveRecord::Migration[5.2]
-  def up
+  def change
     change_column :posts, :tag_id, :integer
-  end
-
-  def down
-    change_column :posts, :tag_id, :string
   end
 end
