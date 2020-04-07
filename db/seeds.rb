59e60d73d02b9ca85beb2ac80b2afa9fd2ce84a8
Duy Lai <duyklai@gmail.com>
2020-04-07 15:09:37 -0500

    Add intial setup seed for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: 'adminUser', email: 'admin@example.com', password: 'password', admin: true)
+
+tag = Tag.create(name: 'general')
+
+Post.create(user_id: user.id, points: 1, tag_id: tag.id, title: 'Welcome to Readit', body: 'Welcome to Readit! \n Disclaimer: This website is not associated in anyway with the real Reddit. This website was made to showcase my web-developing abilities should not be used to make a real Reddit account.')
